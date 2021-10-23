e9325432719ac7ba9669994e0a4c29b7457be451
foreverwander <lydia.liu@mail.utoronto.ca>
2021-10-22 20:13:50 -0400

    calculate adjustment, plot projection with proposed

--- Analysis_2020-21/projection_2021-22.R
+++ Analysis_2020-21/projection_2021-22.R
@@ -6,6 +6,53 @@ library(BoutrosLab.plotting.general)
 
 ## HELPER #########################################################################################
 
+forward_compound <- function(value, percent){
+
+    for (j in percent){
+        value <- value * (1+j)
+    }
+
+    return(value)
+
+}
+
+backward_compound <- function(value, percent){
+
+    for (j in rev(percent)){
+        value <- value / (1 + j)
+    }
+
+    return(value)
+
+}
+
+convert_year_to_2019 <- function(value_df, cpi_df){
+
+    before_2019 <- value_df[value_df$year < 2019, ]
+
+    for (i in 1:nrow(before_2019)){
+
+        before_2019$value[i] <- forward_compound(before_2019$value[i],
+            cpi_df$value[before_2019$year[i] < cpi_df$year & cpi_df$year <= 2019])
+
+    }
+
+
+    at_2019 <- value_df[value_df$year == 2019, ]
+
+    after_2019 <- value_df[value_df$year > 2019, ]
+
+    for (i in 1:nrow(after_2019)){
+
+        after_2019$value[i] <- backward_compound(after_2019$value[i],
+            cpi_df$value[cpi_df$year > 2019 & cpi_df$year <= after_2019$year[i]])
+
+    }
+
+
+    return(c(before_2019$value, at_2019$value, after_2019$value))
+}
+
 ## DATA ###########################################################################################
 
 work_dir <- '~/Documents/grc_data/Projection/'
@@ -17,38 +64,71 @@ setwd(work_dir)
 projection_file <- "2021 External Stats.xlsx - TSV for R.tsv"
 projection <- read.delim(projection_file, as.is=T)
 
+## PROJECT ########################################################################################
+
+# sanity check
+limat_year_df <- projection[, c('Year', 'LIM.AT.Year')]
+cpi_df <- projection[, c('Year', 'CPI.Percent')]
+colnames(limat_year_df) <- c('year', 'value')
+colnames(cpi_df) <- c('year', 'value')
+stopifnot(all.equal(projection$LIM.AT.2019, convert_year_to_2019(limat_year_df, cpi_df)))
+
+# phd
+phd_year_df <- projection[, c('Year', 'PhD.Year')]
+colnames(phd_year_df) <- c('year', 'value')
+projection$PhD.2019 <- convert_year_to_2019(phd_year_df, cpi_df)
+
+# msc
+msc_year_df <- projection[, c('Year', 'MSc.Year')]
+colnames(msc_year_df) <- c('year', 'value')
+projection$MSc.2019 <- convert_year_to_2019(msc_year_df, cpi_df)
+
 ## PLOT ###########################################################################################
+
+projection <- projection[, colnames(projection) !=  'CPI.Percent']
 
 projection$order <- 1:nrow(projection)
 projection_stack <- reshape2::melt(projection, id.vars = c('Year', 'order'))
 
-projection_stack$group <- gsub('(.*)\\.(.*)', '\\1', projection_stack$variable)
-projection_stack$type <- gsub('(.*)\\.(.*)', '\\2', projection_stack$variable)
-projection_stack$group <- factor(projection_stack$group, c('LIM.AT', 'PhD', 'MSc'))
-projection_stack$type <- factor(projection_stack$type, c('2019', 'Year'))
+# separate out stipend projections
+projection_stack$variable <- as.character(projection_stack$variable)
+projected_val <- projection_stack$Year > 2021 & grepl('PhD|MSc', projection_stack$variable)
+projection_stack$variable[projected_val] <- paste0(projection_stack$variable[projected_val], '.Projection')
+# add another 2021 for plotting
+val_2021 <- projection_stack$Year == 2021 & grepl('PhD|MSc', projection_stack$variable)
+projection_2021 <- projection_stack[val_2021, ]
+projection_2021$variable <- paste0(projection_2021$variable, '.Projection')
+projection_stack <- rbind(projection_stack, projection_2021)
+
+projection_stack <- projection_stack[order(projection_stack$variable, projection_stack$order), ]
+
+projection_stack$variable <- factor(projection_stack$variable,
+    c('LIM.AT.Year', 'LIM.AT.2019', 'PhD.Year', 'PhD.2019', 'MSc.Year', 'MSc.2019',
+        'PhD.Year.Projection', 'PhD.2019.Projection',
+        'MSc.Year.Projection', 'MSc.2019.Projection'))
 
 # legend
 projection_key <- list(
     text = list(
         lab = c('LIM-AT Year Adjusted', 'LIM-AT 2019 Constant $',
         		'PhD Living Allowance', 'PhD Living Allowance 2019 Constant $',
-        		'MSc Living Allowance', 'MSc Living Allowance 2019 Constant $'),
+        		'MSc Living Allowance', 'MSc Living Allowance 2019 Constant $',
+                'Proposed Increase',
+                'Proposed Increase 2019 Constant $'),
         cex = 0.8,
-        col = rep(default.colours(3, 'spiral.morning'), each = 2)
+        col = c(rep(c("#542A85", "#B92F5A", "#D9713E"), each = 2), 'red', '#F8B4E3')
         ),
     lines = list(
-        lty = rep(c(1, 2), 3),
-        col = rep(default.colours(3, 'spiral.morning'), each = 2),
+        lty = rep(c(1, 2), 4),
+        col = c(rep(c("#542A85", "#B92F5A", "#D9713E"), each = 2), 'red', '#F8B4E3'),
         lwd = 2,
-        cex = 0.5
-        ),
-    points = list(
-        pch = rep(c(1, 0, 2), each = 2),
-        col = rep(default.colours(3, 'spiral.morning'), each = 2),
         cex = 0.5
         ),
     padding.text = 2
     )
+
+line_cols <- c(rep(c("#542A85", "#B92F5A", "#D9713E"), each = 2),
+                rep(c('red', '#F8B4E3'), 2))
 
 create.scatterplot(
     main = 'Living Allowance vs. LIM-AT Projection',
@@ -66,12 +146,13 @@ create.scatterplot(
     xaxis.cex = 0.8,
     xaxis.tck = c(0.5, 0),
     yaxis.cex = 0.8,
-    ylimits = c(16000, 31000),
+    ylimits = c(16000, 41000),
     yaxis.tck = c(0.5, 0),
     type = c('p', 'l'),
-    pch = rep(c(1, 0, 2), each = 2),
-    lty = rep(c(2, 1), 3),
-    col = rep(default.colours(3, 'spiral.morning'), each = 2),
+    pch = rep(c(19, 15, 17, 15, 17), each = 2),
+    lty = rep(c(1, 2), 10),
+    col = line_cols,
+    lwd = c(rep(1, 6), 2, 1, 2, 1),
     # abline.v = breakpoints,
     # abline.col = 'grey70',
     # abline.lwd = 1,
@@ -86,9 +167,9 @@ create.scatterplot(
 	add.rectangle = TRUE,
 	xleft.rectangle = c(6, 9),
 	ybottom.rectangle = c(0, 0),
-	xright.rectangle = c(9, 13),
-	ytop.rectangle = rep(35000, 2),
-	col.rectangle = c('yellowgreen', 'grey75'),
+	xright.rectangle = c(9, 50),
+	ytop.rectangle = rep(50000, 2),
+	col.rectangle = c('#99C19A', '#FAE5A1'),
 	alpha.rectangle = c(0.25, 0.5),
     # LEGEND
     legend = list(
@@ -99,8 +180,10 @@ create.scatterplot(
             y = 0.95)
         ),
     use.legacy.settings = TRUE,
-    filename = paste0(date, '_projection_yearly_vs_2019', '.pdf'),
-    width = 6,
+    filename = paste0(date, '_projection_0635_5_years_then_follow', '.pdf'),
+    width = 10,
     height = 6,
     resolution = 300
     )
+
+write.table(projection_stack, paste0(date, '_projection_0635_5_years_then_follow.tsv'), col.names=T, row.names=F, quote=T, sep='\t')
